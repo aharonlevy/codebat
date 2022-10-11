b541bc746ca5970d02611c5d7ab0f58d950fd13b
Aharon Levy <mr.aharon.levy@gmail.com>
2022-10-11 17:46:01 +0300

    initialized warmup_2 added string_times

---
+++ warmup_2.py
@@ -0,0 +1,8 @@
+
+
+def string_times(words, num):
+    ''''
+    Given a string and a non-negative int n,
+     return a larger string that is n copies of the original string.
+    '''
+    return words*num
